39279aa3f500b07a233009e5490a0198d717be4f
eric-blue <ericburnt@gmail.com>
2023-09-17 09:50:08 -0700

    shadows, tree fixes, begin work on char sprite

--- src/models/character-flipbook.ts
+++ src/models/character-flipbook.ts
@@ -1,5 +1,7 @@
-import {Sprite, TextureLoader, SpriteMaterial, Texture, NearestFilter, Mesh} from 'three';
+import {Sprite, TextureLoader, SpriteMaterial, Texture, NearestFilter, Mesh, SRGBColorSpace} from 'three';
 import {Direction} from '../types';
+
+const IDLE_SPEED = 1.85;
 
 export class SpriteFlipbook {
   private player: Mesh;
@@ -18,7 +20,7 @@ export class SpriteFlipbook {
   private playSpriteIndices: number[] = [];
   sprite: Sprite;
 
-  constructor(player: Mesh, texture: string, tilesHorizontal = 8, tilesVertical = 8) {
+  constructor(player: Mesh, texture: string, tilesHorizontal = 8, tilesVertical = 16) {
     this.player = player;
     this.tilesHorizontal = tilesHorizontal;
     this.tilesVertical = tilesVertical;
@@ -28,7 +30,7 @@ export class SpriteFlipbook {
     this.sprite = sprite;
 
     this.update(0);
-    this.loop(IDLE_DOWN.tiles, 0.45);
+    this.loop(IDLE_DOWN.tiles, IDLE_SPEED);
     
     this.player.add(this.sprite);
   }
@@ -60,7 +62,8 @@ export class SpriteFlipbook {
 
     if (this.currentAnimation.key !== nextAnimation.key) {
       this.currentAnimation = nextAnimation;
-      this.loop(this.currentAnimation.tiles, 0.45);
+      const speed = direction.includes('idle') ? IDLE_SPEED : 0.85;
+      this.loop(this.currentAnimation.tiles, speed);
     }
   }
 
@@ -80,12 +83,12 @@ function loadSprite(loadedTexture: Texture, tilesHorizontal: number, tilesVertic
 
   map.magFilter = NearestFilter;
   map.repeat.set(1 / tilesHorizontal, 1 / tilesVertical);
-  map.offset.set(0.125, 0.875); // this shouldn't be necessary
+  map.colorSpace = SRGBColorSpace;
 
   const material = new SpriteMaterial({map});
   const sprite = new Sprite(material);
   sprite.position.set(0, 0.5, 0);
-  sprite.scale.set(2, 2, 1);
+  sprite.scale.set(1.5, 1.5, 1);
 
   return {map, sprite};
 }
@@ -97,14 +100,15 @@ export class SpriteAnimation {
   ) {}
 }
 
-export const IDLE_UP = new SpriteAnimation([0, 1, 2, 3], 'IDLE_UP'); // TBD
+export const IDLE_UP = new SpriteAnimation([48, 49, 50, 51], 'IDLE_UP');
 export const IDLE_LEFT = new SpriteAnimation([24, 25, 26, 27], 'IDLE_LEFT');
-export const IDLE_DOWN = new SpriteAnimation([24, 25, 26, 27], 'IDLE_DOWN'); // TBD
+export const IDLE_DOWN = new SpriteAnimation([72, 73, 74, 75], 'IDLE_DOWN');
 export const IDLE_RIGHT = new SpriteAnimation([0, 1, 2, 3], 'IDLE_RIGHT');
 
-export const RUN_UP = new SpriteAnimation([8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 21], 'RUN_UP'); // TBD
+export const RUN_UP = new SpriteAnimation([56, 57, 58, 59, 60, 61, 64, 65, 66, 67, 68, 69], 'RUN_UP');
 export const RUN_LEFT = new SpriteAnimation([32, 33, 34, 35, 36, 37, 40, 41, 42, 43, 44, 45], 'RUN_LEFT');
-export const RUN_DOWN = new SpriteAnimation([32, 33, 34, 35, 36, 37, 40, 41, 42, 43, 44, 45], 'RUN_DOWN'); // TBD
+export const RUN_DOWN = new SpriteAnimation([80, 81, 82, 83, 84, 85, 88, 89, 90, 91, 92, 93], 'RUN_DOWN');
+// export const RUN_DOWN = new SpriteAnimation([80, 81, 82, 88, 89, 90], 'RUN_DOWN');
 export const RUN_RIGHT = new SpriteAnimation([8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 21], 'RUN_RIGHT');
 
 const animations = {
